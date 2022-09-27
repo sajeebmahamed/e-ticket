77f670dd5505346c9a6b327c37917238b096ec16
sajeebmahamed <sajeebahamed6@gmail.com>
2022-09-28 00:05:47 +0600

    Auth service initial setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,19 @@ import {json} from "body-parser"
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there!')
+})
+
+app.get('/api/users/user', (req, res) => {
+    res.send('Hi there Sajeeb!')
+})
+
+
+app.get('/api/users/ok', (req, res) => {
+    res.send('Hi there ok!')
+})
+
 const port = 3000
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`)
