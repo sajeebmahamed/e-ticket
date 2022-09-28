040d81f12d9140f262946ef78bb5391bd65e316a
sajeebmahamed <sajeebahamed6@gmail.com>
2022-09-29 02:46:20 +0600

    ADD: handle errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if(err instanceof RequestValidationError) {
+    if(err instanceof CustomError) {
         return res.status(err.statusCode).send({ errors: err.serializeErrors() })
     }
-    if (err instanceof DatabaseConnectionError) {
-        return res.status(err.statusCode).send({ errors: err.serializeErrors() })
-    }
+    
     res.status(400).send({
         errors: [{ message: 'Something went wrong' }]
     })
