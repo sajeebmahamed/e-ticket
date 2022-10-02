5a0b1605cefd58c500fc65c9da4d6588ed88e88d
sajeebmahamed <sajeebahamed6@gmail.com>
2022-10-02 14:50:39 +0600

    fixed error issues

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,40 @@
-import express from 'express'
-// import 'express-async-errors'
-import {json} from "body-parser"
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/singup'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
-const app = express()
-app.use(json())
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-app.all('*', async (req, res, next) => {
-    next(new NotFoundError())
-})
+const app = express();
+app.use(json());
 
-// app.all('*', async (req, res, next) => {
-//     throw new NotFoundError()
-// })
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.use(errorHandler)
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`Listening on port ${port}.`)
-})
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {});
+        console.log('Connected to MongoDb');
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!!!!!!');
+    });
+};
+
+start();
