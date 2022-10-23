722b90219b5a4f66d95eb3c4196178ed11f3ee30
sajeebmahamed <sajeebahamed6@gmail.com>
2022-10-23 10:56:03 +0600

    Auth service test implimentation done

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,8 +4,8 @@ import { currentUser } from '../middlewares/current-user';
 import { requireAuth } from '../middlewares/require-auth';
 const router = express.Router()
 
-router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
-    res.send(req.currentUser);
-})
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+    res.send({ currentUser: req.currentUser || null });
+});
 
 export {router as currentUserRouter}
